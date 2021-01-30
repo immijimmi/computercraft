08999b128cdb39fe6a14cbd108fd2be91c12d9d3
Immijimmi <immijimmi1@gmail.com>
2021-01-30 21:47:10 +0000

    Added WIP folder, completed move()

--- computercraft/move.py
+++ computercraft/move.py
@@ -1,14 +1,27 @@
 def move(move_str):
+    def apply_turns(reverse=False):
+        if not reverse:
+            if move_str == "left":
+                turtle.turnLeft()
+            elif move_str == "right":
+                turtle.turnRight()
+            elif move_str == "back":
+                turtle.turnRight()
+                turtle.turnRight()
+
+        else:
+            if move_str == "left":
+                turtle.turnRight()
+            elif move_str == "right":
+                turtle.turnLeft()
+            elif move_str == "back":
+                turtle.turnLeft()
+                turtle.turnLeft()
+
     if move_str not in {"up", "down", "left", "right", "forward", "back"}:
         raise ValueError(move_str)
 
-    if move_str == "left":
-        turtle.turnLeft()
-    elif move_str == "right":
-        turtle.turnRight()
-    elif move_str == "back":
-        turtle.turnRight()
-        turtle.turnRight()
+    apply_turns()
 
     if move_str == "up":
         move_command = turtle.up
@@ -22,17 +35,14 @@ def move(move_str):
 
     for i in range(100):  # and i would try 100 times and i would try 100 more
             if move_command():
-                break
+                return
 
             if i == 99:
-                if move_str == "left":
-                    turtle.turnRight()
-                elif move_str == "right":
-                    turtle.turnLeft()
-                elif move_str == "back":
-                    turtle.turnLeft()
-                    turtle.turnLeft()
+                apply_turns(reverse=True)
+                return False
+
+            if clean_inventory() is False:
+                apply_turns(reverse=True)
                 return False
 
             dig_command()
-            clean_inventory()
