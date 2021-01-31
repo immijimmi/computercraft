838087f7d09e150e9bc4bef05f9b92d2cf88b62b
Immijimmi <immijimmi1@gmail.com>
2021-01-31 09:17:03 +0000

    Added to constants

--- computercraft/constants.py
+++ computercraft/constants.py
@@ -42,9 +42,21 @@ class Mock:
         def getFuelLimit(self):
             pass
 
+        def getFuelLevel(self):
+            pass
+
+        def inspect(self):
+            pass
+
+        def inspectUp(self):
+            pass
+
+        def inspectDown(self):
+            pass
+
 
 class Constants:
-    non_valuables = {
+    non_valuables = {  # Used to filter items out of the inventory
         # Overworld
         # "minecraft:sand",
         "minecraft:gravel",
@@ -59,6 +71,24 @@ class Constants:
         "minecraft:magma_block",
         "minecraft:blackstone",
         "minecraft:soul_sand"
+    }
+
+    valuables = {  # Used to pick blocks to mine
+        # Overworld
+        "minecraft:diamond_ore",
+        "minecraft:redstone_ore",
+        "minecraft:iron_ore",
+        "minecraft:gold_ore",
+        "minecraft:coal_ore",
+        "minecraft:emerald_ore",
+        "minecraft:lapis_lazuli_ore",
+        "minecraft:obsidian",
+
+        # Nether
+        "minecraft:ancient_debris",
+        "minecraft:nether_gold_ore",
+        "minecraft:glowstone",
+        "minecraft:nether_quartz_ore"
     }
 
     empty_slots_required = 2
