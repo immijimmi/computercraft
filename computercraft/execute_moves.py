7091d0a92de0e510d8ba34d753449d758381e73f
Immijimmi <immijimmi1@gmail.com>
2021-01-31 07:37:09 +0000

    Added execute_moves()

---
+++ computercraft/execute_moves.py
@@ -0,0 +1,14 @@
+from typing import Sequence
+
+from .try_move import try_move
+
+
+def execute_moves(moves: Sequence[str]) -> None:
+    """
+    Basic batch function for try_move() - does not complete any additional work between moves nor
+    make fuel checks/refuel
+    """
+
+    for move in moves:
+        if not try_move(move):
+            raise RuntimeError("Unable to complete move sequence.")
