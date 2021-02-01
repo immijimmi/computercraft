608915e921c3f92567a5084214a989d30d077849
Immijimmi <immijimmi1@gmail.com>
2021-02-01 03:33:49 +0000

    Translated constants to lua

---
+++ computercraft/constants.lua
@@ -0,0 +1,84 @@
+function empty()
+end
+
+
+function empty_2(arg)
+end
+
+
+mock = {
+    turtle = {
+        up = empty,
+        down = empty,
+        forward = empty,
+        back = empty,
+        turnLeft = empty,
+        turnRight = empty,
+        dig = empty,
+        digUp = empty,
+        digDown = empty,
+        getItemDetail = empty,
+        drop = empty,
+        getSelectedSlot = empty,
+        getFuelLimit = empty,
+        getFuelLevel = empty,
+        inspect = empty,
+        inspectUp = empty,
+        inspectDown = empty,
+
+        select = empty_2
+    }
+}
+
+constants = {
+    non_valuables = {  -- Used to filter items out of the inventory
+        -- Overworld
+        -- ["minecraft:sand"]=true,
+        ["minecraft:gravel"] = true,
+        ["minecraft:granite"] = true,
+        ["minecraft:diorite"] = true,
+        ["minecraft:andesite"] = true,
+        ["minecraft:dirt"] =true,
+        ["minecraft:cobblestone"] = true,
+
+        -- Nether
+        ["minecraft:netherrack"] = true,
+        ["minecraft:magma_block"] = true,
+        ["minecraft:blackstone"] = true,
+        ["minecraft:soul_sand"] = true
+    }
+
+    valuables = {  -- Used to pick blocks to mine
+        -- Overworld
+        ["minecraft:diamond_ore"] = true,
+        ["minecraft:redstone_ore"] = true,
+        ["minecraft:iron_ore"] = true,
+        ["minecraft:gold_ore"] = true,
+        ["minecraft:coal_ore"] = true,
+        ["minecraft:emerald_ore"] = true,
+        ["minecraft:lapis_lazuli_ore"] = true,
+        ["minecraft:obsidian"] = true,
+
+        -- Nether
+        ["minecraft:ancient_debris"] = true,
+        ["minecraft:nether_gold_ore"] = true,
+        ["minecraft:glowstone"] = true,
+        ["minecraft:nether_quartz_ore"] = true
+    }
+
+    empty_slots_required = 2
+
+    reversed_moves = {
+        left = "right",
+        right = "left",
+        up = "down",
+        down = "up",
+        forward = "forward",
+        back = "back"
+    }
+}
+
+return {
+    constants = constants,
+    mock = mock
+}
