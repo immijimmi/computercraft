4c5878b183e4580c46752a8e0ab2d51401bb3b01
Immijimmi <immijimmi1@gmail.com>
2021-02-17 22:23:51 +0000

    Added find_item

---
+++ building/find_item.lua
@@ -0,0 +1,19 @@
+function find_item(name)
+    local result
+    local current_slot_index = turtle.getSelectedSlot()
+
+    for slot_index=1,16 do
+        turtle.select(slot_index)
+
+        local details = turtle.getItemDetail()
+        if details and details["name"] == name then
+            result = slot_index
+            break
+        end
+    end
+
+    turtle.select(current_slot_index)
+    return result
+end
+
+return find_item
