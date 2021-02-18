39d7b85232fce4cc764e4bc2e5721145e148e2bc
Immijimmi <immijimmi1@gmail.com>
2021-02-18 19:02:40 +0000

    Fixed fuel check calculation in try_refuel and added more comment detail

--- mining/try_refuel.lua
+++ mining/try_refuel.lua
@@ -1,7 +1,11 @@
 function try_refuel(offset_amount)
     -- TODO: Add logic for refueling
 
-    if turtle.getFuelLevel() < offset_amount * 2 then  -- Expecting at least 50% efficiency
+    --[[
+    Below calc assumes that you want to return to where you were before backtracking to refuel,
+    and be able to make at least 1 more move before needing to backtrack once again
+    --]]
+    if turtle.getFuelLevel() < (offset_amount * 2) + 2 then
         return False
     else
         return True
