5180cb24522230b57aa06d91dee271f5032a1f92
Immijimmi <immijimmi1@gmail.com>
2021-02-18 19:27:34 +0000

    Modified deploy_turtle to use fuel_required

--- management/deploy_turtle.lua
+++ management/deploy_turtle.lua
@@ -2,27 +2,31 @@ local try_excavate = require("mining.try_excavate")
 local execute_moves = require("mining.execute_moves")
 local execute_reversed_moves = require("mining.execute_reversed_moves")
 local try_refuel = require("mining.try_refuel")
+local fuel_required = require("mining.fuel_required")
 
 
-function deploy_turtle(prior_offset)
-    if prior_offset == nil then
-        prior_offset = {}
+function deploy_turtle(prior_moves)
+    if prior_moves == nil then
+        prior_moves = {}
     end
 
-    if not try_excavate({[1]="forward"}, prior_offset) then
+    if not try_excavate({[1]="forward"}, prior_moves) then
         error("unable to clear space to place the turtle")
     end
-    if not try_excavate({[1]="left", [2]="right"}, prior_offset) then
+    if not try_excavate({[1]="left", [2]="right"}, prior_moves) then
         error("unable to clear space to place the disk drive")
     end
 
-    local remaining_fuel_required = #prior_offset + 4
+    local fuel_spent = fuel_required(prior_moves)
+    local fuel_moves_remaining = 4  -- Hardcoded value for how many moves are to be carried out below
+    local remaining_fuel_required = fuel_spent + fuel_moves_remaining
+
     if turtle.getFuelLevel() < remaining_fuel_required then  -- If fuel limit is reached
-        execute_reversed_moves(prior_offset)
-        if not try_refuel(remaining_fuel_required) then
+        execute_reversed_moves(prior_moves)
+        if not try_refuel(remaining_fuel_required + fuel_spent, false) then
             error("insufficient fuel")
         end
-        execute_moves(prior_offset)
+        execute_moves(prior_moves)
     end
 
     local current_slot_index = turtle.getSelectedSlot()
