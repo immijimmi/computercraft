7cc7efd85fa9bfd45319ae9e186ffbc68dc836e5
Immijimmi <immijimmi1@gmail.com>
2021-02-19 08:24:08 +0000

    Fixed string formatting

--- computercraft/management/deploy_strip_miners.lua
+++ computercraft/management/deploy_strip_miners.lua
@@ -7,7 +7,7 @@ function deploy_strip_miners(amount, curr_depth, resource, distance)
     file_contents = file_contents.."local orient = require('turtle.orient')\n"
     file_contents = file_contents.."local try_strip_mine = require('mining.try_strip_mine')\n\n"
     file_contents = file_contents.."orient()\n"
-    file_contents = file_contents.."try_strip_mine(%s, %s, %s)"
+    file_contents = file_contents.."try_strip_mine(%d, '%s', %d)"
     file_contents = string.format(file_contents, curr_depth, resource, distance)
 
     set_startup_replace(file_contents, true)
