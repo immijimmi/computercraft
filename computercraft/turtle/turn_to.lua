818a5a7cd8805ce8af48945b00aa21634d171474
Immijimmi <immijimmi1@gmail.com>
2021-02-21 20:26:39 +0000

    Syntax Fix

--- computercraft/turtle/turn_to.lua
+++ computercraft/turtle/turn_to.lua
@@ -25,6 +25,7 @@ function turn_to(move_or_direction, is_reverse)
             turtle.turnRight()
         elseif move_or_direction == "right" or move_or_direction == "turnRight" then
             turtle.turnLeft()
+        end
     end
 
     if move_or_direction == "back" then
