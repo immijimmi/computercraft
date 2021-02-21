e056ddbc01fc02325ca76fbbb941e3994976d459
Immijimmi <immijimmi1@gmail.com>
2021-02-21 06:04:49 +0000

    Updated constants for valuables and non valuables

--- computercraft/mining/constants.lua
+++ computercraft/mining/constants.lua
@@ -1,44 +1,46 @@
 local constants = {
     non_valuables = {  -- Used to filter items out of the inventory
         -- Overworld
-        -- ["minecraft:sand"] = true,
-        -- ["minecraft:gravel"] = true,
-        ["minecraft:granite"] = true,
-        ["minecraft:diorite"] = true,
-        ["minecraft:andesite"] = true,
-        ["minecraft:dirt"] = true,
-        ["minecraft:cobblestone"] = true,
+        ["minecraft:sand"]=true,
+        ["minecraft:gravel"]=true,
+        ["minecraft:granite"]=true,
+        ["minecraft:diorite"]=true,
+        ["minecraft:andesite"]=true,
+        ["minecraft:dirt"]=true,
+        ["minecraft:cobblestone"]=true,
+        ["minecraft:oak_planks"]=true,
 
         -- Nether
-        ["minecraft:netherrack"] = true,
-        -- ["minecraft:magma_block"] = true,
-        ["minecraft:blackstone"] = true,
-        -- ["minecraft:soul_sand"] = true
+        ["minecraft:netherrack"]=true,
+        ["minecraft:magma_block"]=true,
+        ["minecraft:blackstone"]=true,
+        ["minecraft:soul_sand"]=true
     },
 
     valuables = {  -- Used to pick blocks to mine
         -- Overworld
-        ["minecraft:diamond_ore"] = true,
-        ["minecraft:redstone_ore"] = true,
-        ["minecraft:iron_ore"] = true,
-        ["minecraft:gold_ore"] = true,
-        ["minecraft:coal_ore"] = true,
-        ["minecraft:emerald_ore"] = true,
-        ["minecraft:lapis_lazuli_ore"] = true,
-        ["minecraft:obsidian"] = true,
+        ["minecraft:diamond_ore"]=true,
+        ["minecraft:redstone_ore"]=true,
+        ["minecraft:iron_ore"]=true,
+        ["minecraft:gold_ore"]=true,
+        ["minecraft:coal_ore"]=true,
+        ["minecraft:emerald_ore"]=true,
+        ["minecraft:lapis_ore"]=true,
+        ["minecraft:obsidian"]=true,
+        ["minecraft:crying_obsidian"]=true,
 
         -- Nether
-        ["minecraft:ancient_debris"] = true,
-        ["minecraft:nether_gold_ore"] = true,
-        ["minecraft:glowstone"] = true,
-        ["minecraft:nether_quartz_ore"] = true,
+        ["minecraft:ancient_debris"]=true,
+        ["minecraft:nether_gold_ore"]=true,
+        ["minecraft:glowstone"]=true,
+        ["minecraft:nether_quartz_ore"]=true,
 
         -- Ars Nouveau
-        ["ars_nouveau:arcane_ore"] = true,
+        ["ars_nouveau:arcane_ore"]=true,
 
         -- Applied Energistics 2 (WIP)
-        ["appliedenergistics2:sky_stone_block"] = true,
-        ["appliedenergistics2:charged_quartz_ore"] = true
+        ["appliedenergistics2:sky_stone_block"]=true,
+        ["appliedenergistics2:charged_quartz_ore"]=true
     },
 
     empty_slots_required = 2,
