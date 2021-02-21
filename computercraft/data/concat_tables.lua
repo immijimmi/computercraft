83db330740ba47691af9d2b615d23b45cbd2f066
Immijimmi <immijimmi1@gmail.com>
2021-02-21 19:41:52 +0000

    Added concat_tables()

---
+++ computercraft/data/concat_tables.lua
@@ -0,0 +1,16 @@
+function concat_tables(a, b)
+    local result = {}
+
+    for key, value in pairs(a) do
+        result[key] = value
+    end
+
+    for key, value in ipairs(b) do
+        result[key] = value
+    end
+
+    return result
+end
+
+
+return concat_lists
