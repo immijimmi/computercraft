b0fec5ae3812665ab02df7de9b45c6db20f84b2c
Immijimmi <immijimmi1@gmail.com>
2021-02-23 05:56:16 +0000

    Updated try_clean_inventory to receive a table of items to remove

--- computercraft/mining/try_clean_inventory.lua
+++ computercraft/mining/try_clean_inventory.lua
@@ -1,8 +1,7 @@
-local constants = require("mining.constants")
-local has_free_slots = require("turtle.has_free_slots")
+local concat_tables = require("data.concat_tables")
 
 
-function try_clean_inventory(empty_slots_required, is_eager)
+function try_clean_inventory(items_to_remove, empty_slots_required, is_eager)
     --[[
     If the inventory does not have enough empty slots after cleaning,
     this function returns False
@@ -13,10 +12,19 @@ function try_clean_inventory(empty_slots_required, is_eager)
         is_eager = false
     end
 
+    for name, count in pairs(items_to_remove) do
+        assert(
+            count == true or
+                (type(count) == "number" and count > 0),
+            "items to be removed must be provided as a table of the item's string ID to the >0 amount to be removed (or true if all should be removed)"
+        )
+    end
+    items_to_remove = concat_tables({}, items_to_remove)  -- Making a working copy
+
     local current_slot_index = turtle.getSelectedSlot()
 
     local empty_slots = 0
-    local non_valuables_indexes = {}
+    local removable_indexes = {}
 
     for slot_index=1,16 do
         turtle.select(slot_index)
@@ -24,16 +32,35 @@ function try_clean_inventory(empty_slots_required, is_eager)
 
         if item_detail == nil then
             empty_slots = empty_slots+1
-        elseif constants.non_valuables[item_detail["name"]] then
-            non_valuables_indexes[#non_valuables_indexes+1] = slot_index
+        elseif items_to_remove[item_detail["name"]] ~= nil then
+            removable_indexes[#removable_indexes+1] = slot_index
         end
     end
 
-    for _, non_valuable_index in ipairs(non_valuables_indexes) do
+    for _, removable_index in ipairs(removable_indexes) do
         if is_eager or (empty_slots < empty_slots_required) then
-            turtle.select(non_valuable_index)
-            turtle.drop()
-            empty_slots = empty_slots+1
+            turtle.select(removable_index)
+            local item_detail = turtle.getItemDetail()
+            local item_name = item_detail["name"]
+            local item_count = item_detail["count"]
+
+            local remove_value = items_to_remove[item_name]
+
+            if remove_value == true then
+                turtle.drop()
+                empty_slots = empty_slots+1
+            elseif remove_value > item_count then
+                turtle.drop()
+                empty_slots = empty_slots+1
+                items_to_remove[item_name] = remove_value - item_count
+            elseif remove_value == item_count then
+                turtle.drop()
+                empty_slots = empty_slots+1
+                items_to_remove[item_name] = nil
+            else
+                turtle.drop(remove_value)
+                items_to_remove[item_name] = nil
+            end
         end
     end
 
