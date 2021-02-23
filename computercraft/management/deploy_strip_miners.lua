7ba27a5ba752dfbd5fc4a18c0721bb88b8952926
Immijimmi <immijimmi1@gmail.com>
2021-02-23 22:19:27 +0000

    Added missing keep_non_valuables param in deploy_strip_miners()

--- computercraft/management/deploy_strip_miners.lua
+++ computercraft/management/deploy_strip_miners.lua
@@ -1,7 +1,12 @@
 local handle_deployment = require("management.handle_deployment")
 
 
-function deploy_strip_miners(amount, curr_depth, resource, distance)
+function deploy_strip_miners(amount, curr_depth, resource, distance, keep_non_valuables)
+    -- Default values
+    if keep_non_valuables == nil then
+        keep_non_valuables = false
+    end
+
     local file_contents = [[
 shell.run("delete startup.lua")
 
@@ -9,9 +14,9 @@ local orient = require("turtle.orient")
 local try_strip_mine = require("mining.try_strip_mine")
 
 orient()
-try_strip_mine(%d, "%s", %d)
+try_strip_mine(%d, "%s", %d, %s)
 ]]
-    file_contents = string.format(file_contents, curr_depth, resource, distance)
+    file_contents = string.format(file_contents, curr_depth, resource, distance, keep_non_valuables)
 
     local moves_between = {"right", "forward", "forward", "turnLeft"}
 
