8637ec45b330dbf9cf47e878b8bb6d1736d0c679
Immijimmi <immijimmi1@gmail.com>
2021-02-23 17:52:16 +0000

    merged_item_counts() can now receive any amount of item count tables

--- computercraft/data/merged_item_counts.lua
+++ computercraft/data/merged_item_counts.lua
@@ -1,15 +1,13 @@
-function merged_item_counts(a, b)
+function merged_item_counts(...)
     local result = {}
 
-    for item_name, item_count in pairs(a) do
-        result[item_name] = item_count
-    end
-
-    for item_name, item_count in pairs(b) do
-        if result[item_name] == nil then
-            result[item_name] = item_count
-        else
-            result[item_name] = result[item_name] + item_count
+    for _, item_count_table in ipairs(...) do
+        for item_name, item_count in pairs(item_count_table) do
+            if result[item_name] == nil then
+                result[item_name] = item_count
+            else
+                result[item_name] = result[item_name] + item_count
+            end
         end
     end
 
