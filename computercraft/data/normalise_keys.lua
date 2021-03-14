65b103054964a9df8eeee2745e312f00aa5f792c
immijimmi <immijimmi1@gmail.com>
2021-03-14 10:42:22 +0000

    Minor style change

--- computercraft/data/normalise_keys.lua
+++ computercraft/data/normalise_keys.lua
@@ -1,10 +1,10 @@
 function normalise_keys(table_a, table_b)
-    for key, value in pairs(table_a) do
+    for key, _ in pairs(table_a) do
         if table_b[key] == nil then
             table_b[key] = false
         end
     end
-    for key, value in pairs(table_b) do
+    for key, _ in pairs(table_b) do
         if table_a[key] == nil then
             table_a[key] = false
         end
