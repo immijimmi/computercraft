954653da5de6eb87f774d314e4d4a6e18bf961d0
immijimmi <immijimmi1@gmail.com>
2021-05-23 14:55:56 +0100

    Added is_redstone_on

---
+++ computercraft/turtle/is_redstone_on.lua
@@ -0,0 +1,17 @@
+function is_redstone_on(sides, min_value)
+    -- Default values
+    if min_value == nil then
+        min_value = 1  -- Any redstone power value at all will count by default
+    end
+
+    for _, side in pairs(sides) do
+        if rs.getAnalogInput(side) >= min_value then
+            return true
+        end
+    end
+
+    return false
+end
+
+
+return is_redstone_on
