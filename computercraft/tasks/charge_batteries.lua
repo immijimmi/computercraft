b911bd842c09808ff9c9dcda2e93e8e97b8c81e9
immijimmi <immijimmi1@gmail.com>
2021-05-23 16:00:56 +0100

    Made charge_batteries check for redstone input to disable taking new batteries

--- computercraft/tasks/charge_batteries.lua
+++ computercraft/tasks/charge_batteries.lua
@@ -1,13 +1,17 @@
---[[
-A highly specialised stateless routine which expects a container of chargeable batteries above,
-an energy storage block able to receive and charge a single battery in front, and a container with space to receive
-fully charged batteries below.
+local is_redstone_on = require("turtle.is_redstone_on")
 
-Expected use case is with the IC2 and Ender Storage mods, using an ender chest which holds empty batteries above and a different one
-for full batteries below. Paired with a reverse of this routine to unload power where necessary, this makes it possible to teleport power between locations
-and dimensions.
---]]
+
 function charge_batteries()
+    --[[
+    A highly specialised stateless routine which expects a container of chargeable batteries above,
+    an energy storage block able to receive and charge a single battery in front, and a container with space to receive
+    fully charged batteries below.
+
+    Expected use case is with the IC2 and Ender Storage mods, using an ender chest which holds empty batteries above and a different one
+    for full batteries below. Paired with a reverse of this routine to unload power where necessary, this makes it possible to teleport power between locations
+    and dimensions.
+    --]]
+
     local idle_time = 5
 
     turtle.select(1)
@@ -19,11 +23,15 @@ function charge_batteries()
             held_item = turtle.getItemDetail()
 
             if held_item == nil then
-                turtle.suckUp(1)
-                held_item = turtle.getItemDetail()
-
-                if held_item == nil then
+                if is_redstone_on({"back", "left", "right"}) then
                     os.sleep(idle_time)
+                else
+                    turtle.suckUp(1)
+                    held_item = turtle.getItemDetail()
+
+                    if held_item == nil then
+                        os.sleep(idle_time)
+                    end
                 end
             end
 
